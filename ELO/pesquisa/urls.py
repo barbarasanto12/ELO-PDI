97e66ff70ebfe65932f3cd6cbb8ecb16ba1b5451
Bárbara <a2022140478@alumni.iscac.pt>
2025-05-15 11:44:53 +0100

    commit

--- ELO/pesquisa/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pesquisa_view, name='pesquisa'),
-    path('aula/<int:aula_id>/', views.detalhes_aula_view, name='detalhes_aula'),
-]
-
-
